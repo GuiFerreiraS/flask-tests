7c79494b31c1f0e41a6c7cb03ec72cb1e6c06343
vntgufs <guilherme.ferreira@venturus.org.br>
2023-01-11 08:19:42 -0300

    Tags config (many to many relationship)

--- schemas.py
+++ schemas.py
@@ -12,6 +12,11 @@ class PlainStoreSchema(Schema):
     name = fields.Str(required=True)
 
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_oly=True)
+    name = fields.Str(required=True)
+
+
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
@@ -19,9 +24,23 @@ class ItemUpdateSchema(Schema):
 
 
 class ItemSchema(PlainItemSchema):
-    store_id = fields.Int(required=True)
+    store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema, dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema, dump_only=True))
 
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema, dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema, dump_only=True))
+
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
